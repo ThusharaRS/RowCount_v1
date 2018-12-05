e5b6408f81c518f371620fb2c28e400174ec78f7
thusharars <44188247+ThusharaRS@users.noreply.github.com>
2018-12-05 15:11:58 +0530

    Update countRow.R

--- fileCount/R/countRow.R
+++ fileCount/R/countRow.R
@@ -7,18 +7,18 @@
 
 count1 <- function() {
   
-  #data(RLM_Data)
+  data(RLM_Data)
 
-  #RLM_Data_4vs5_0 <- filter(RLM_Data, RLM_Data$KO_Consumption >= 4)[,c(201,26:39,194:200,209:358)]
+  RLM_Data_4vs5_0 <- filter(RLM_Data, RLM_Data$KO_Consumption >= 4)[,c(201,26:39,194:200,209:358)]
 
-  #return(RLM_Data_4vs5_0)
+  return(RLM_Data_4vs5_0)
   
   
   
   
   
-   numRow <- nrow(RLM_Data)
-   return(numRow)
+   #numRow <- nrow(RLM_Data)
+   #return(numRow)
 }
 
 
